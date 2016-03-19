6106a2a0c1f1bdb5d0a4342bd9843073181b7ace
Maira Machado Ladeira <mairamachadoladeira@gmail.com>
2016-03-19 15:54:14 +0100

    basics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by Maira on 3/19/16.
  */
+$('div.class1').css("background-color", "red");
+
+var todoOnClick = function(){
+    var offset = $("#square3").offset();
+    console.log(offset.top);
+    $('html, body').animate({
+          scrollTop: offset.top
+    }, 1000);
+};
+
+$(".class3").on("click", todoOnClick);
